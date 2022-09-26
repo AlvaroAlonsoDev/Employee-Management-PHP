1a8d88aeaa52d99a664cd8dd2d1f96930fe1fb97
Alvaro Alonso <airuritac@gmail.com>
2022-09-26 17:02:27 +0200

    DONE

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -6,23 +6,15 @@ if(isset($_GET["action"]) && $_GET["action"] == "listEmployees"){
     echo saveJson();
 }
 
-    if(isset($userID)){
-        // CALL THE JSON
-        $json_employees = file_get_contents("../resources/employees.json");
-        $employees = json_decode($json_employees, true);
-    
-        foreach($employees as $employee) {
-            if ($userID == $employee['id']) {
-                $user = $employee;
-                break;
-            }
+if(isset($userID)){
+    // CALL THE JSON
+    $json_employees = file_get_contents("../resources/employees.json");
+    $employees = json_decode($json_employees, true);
+
+    foreach($employees as $employee) {
+        if ($userID == $employee['id']) {
+            $user = $employee;
+            break;
         }
     }
-    
-
-
-
-
-
-
-?>
+}
